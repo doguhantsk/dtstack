62d6711fa57a5c5d22d9797f22db9fa4eb3acea2
doguhantsk <s240307013@ankarabilim.edu.tr>
2026-01-30 15:09:53 +0300

    sa

---
+++ assets/emailjs-keys.js
@@ -0,0 +1,8 @@
+// EmailJS keys placeholder - replace these with your real EmailJS values
+// Do NOT commit real keys to a public repo.
+window.EMAILJS_SERVICE_ID = 'service_j96oxki';
+// Owner / contact template (send to site owner)
+window.EMAILJS_TEMPLATE_ID_OWNER = 'template_vz6mp94';
+// Auto-reply template (sent to the user)
+window.EMAILJS_TEMPLATE_ID_AUTOREPLY = 'template_6n13teo';
+window.EMAILJS_PUBLIC_KEY = '-E1BQ3DQoMooRhu8e';
